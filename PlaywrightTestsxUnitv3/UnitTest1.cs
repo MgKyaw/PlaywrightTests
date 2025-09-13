130c282bdf4baeb9bd1e8af6f4a2356df68b3a36
MgKyaw <mgkyaw20112@gmail.com>
2025-09-13 21:32:58 +0700

    feat: Implement xunit v3 unit tests

--- PlaywrightTestsxUnitv3/UnitTest1.cs
+++ PlaywrightTestsxUnitv3/UnitTest1.cs
@@ -1,10 +1,29 @@
-﻿namespace PlaywrightTestsxUnitv3;
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.Xunit.v3;
+using System.Text.RegularExpressions;
 
-public class UnitTest1
+namespace PlaywrightTests;
+
+public class UnitTest1 : PageTest
 {
     [Fact]
-    public void Test1()
+    public async Task HasTitle()
     {
+        await Page.GotoAsync("https://playwright.dev");
 
+        // Expect a title "to contain" a substring.
+        await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+    }
+
+    [Fact]
+    public async Task GetStartedLink()
+    {
+        await Page.GotoAsync("https://playwright.dev");
+
+        // Click the get started link.
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Get started" }).ClickAsync();
+
+        // Expects page to have a heading with the name of Installation.
+        await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Installation" })).ToBeVisibleAsync();
     }
 }
